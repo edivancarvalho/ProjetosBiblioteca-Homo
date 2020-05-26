0a935e95a8370d406ea4bdddf77c908b96759185
Carvalho <edivancarvalho2008@hotmail.com>
2020-05-25 21:23:13 -0300

    Update FormularioSenha.java

--- ProjetosBiblioteca-Homo/src/persistencias/FormularioSenha.java
+++ ProjetosBiblioteca-Homo/src/persistencias/FormularioSenha.java
@@ -6,5 +6,17 @@ package persistencias;
  * @author edivan
  */
 public class FormularioSenha {
+    private String senhas  = "";
+
+    public FormularioSenha() {
+    }
+
+    public FormularioSenha(String senhas){
+        senhas = senhas;
+    }
     
+    public String getSenhas() {
+        return senhas;
+    }
+       //
 }
