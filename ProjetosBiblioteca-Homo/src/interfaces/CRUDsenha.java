dd14438cdfe1a3a65f8f83256f1c15c25385cb61
Carvalho <edivancarvalho2008@hotmail.com>
2020-05-25 22:14:37 -0300

    Update CRUDsenha.java

--- ProjetosBiblioteca-Homo/src/interfaces/CRUDsenha.java
+++ ProjetosBiblioteca-Homo/src/interfaces/CRUDsenha.java
@@ -1,10 +1,18 @@
 package interfaces;
 
+import classesdedados.usuarioESenhas;
+import java.util.ArrayList;
+
 /**
  *
  * @author edivan
  */
+// Create (Criação), Retrieve (Consulta), Update (Atualização) e Delete (Destruição)
 public interface CRUDsenha {
-    void incluirSenha ()
+    void incluirSenha (usuarioESenhas objeto) throws Exception;
+    void alterarSenha (usuarioESenhas antigaSenha, usuarioESenhas novaSenha) throws Exception;
+    void excluirSenha (usuarioESenhas objeto) throws Exception;
+    //public ArrayList<usuarioESenhas> consultaSenha (usuarioESenhas objeto) throws Exception;
+    
     
 }
